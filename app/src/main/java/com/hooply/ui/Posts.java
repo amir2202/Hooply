239eb42d0ab61e5768b1e2447131596f92845edb
amir2202 <aag1u18@soton.ac.uk>
2020-06-20 00:45:59 +0100

    room stuff

--- app/src/main/java/com/hooply/ui/Posts.java
+++ app/src/main/java/com/hooply/ui/Posts.java
@@ -1,4 +1,15 @@
 package com.hooply.ui;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Posts")
 public class Posts {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+
+
+
 }
